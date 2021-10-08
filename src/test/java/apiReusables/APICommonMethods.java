7a0d8d645936d47604c05b75aaab5c005b2ade61
KumarR_Sinha <KumarR_Sinha@cncbinternational.com>
2021-10-08 13:19:58 +0800

    Trying

---
+++ src/test/java/apiReusables/APICommonMethods.java
@@ -0,0 +1,42 @@
+package apiReusables;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class APICommonMethods {
+    public static Map<String,String> authentication  = new HashMap<String , String>();
+
+    public static JSONObject requestBody() {
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("username", "nwkwong");
+        requestParams.put("password", "Aa123456");
+        return requestParams;
+    }
+
+    public static Map headers() {
+        Map header = new HashMap<>();
+        header.put("Content-Type", "application/json");
+        return header;
+
+    }
+
+    public Response sendGetRequest(String baseURI, Map headers) {
+
+        RestAssured.baseURI = baseURI;
+
+        RequestSpecification httpRequest = RestAssured.given();
+        httpRequest.headers(headers);
+
+        Response response = httpRequest.get();
+
+        return response;
+
+    }
+
+}
